603ca6de10035177cbaaf749ae1751c3bfe9160b
mkizhevsk <mkizhevsk@gmail.com>
2020-09-23 23:34:15 +0400

    phone activity

--- app/src/main/java/com/example/myspace/DiaryActivity.java
+++ app/src/main/java/com/example/myspace/DiaryActivity.java
@@ -1,11 +1,11 @@
 package com.example.myspace;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class DiaryActivity extends Activity {
+public class DiaryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
