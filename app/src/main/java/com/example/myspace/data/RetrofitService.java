82bff49d206d68b7ce1a4a97418dafe9085217f0
mkizhevsk <mkizhevsk@gmail.com>
2021-02-12 17:41:24 +0400

    some changes

--- app/src/main/java/com/example/myspace/data/RetrofitService.java
+++ app/src/main/java/com/example/myspace/data/RetrofitService.java
@@ -7,7 +7,6 @@ import retrofit2.http.Query;
 
 public interface RetrofitService {
 
-
     @GET("/data/2.5/weather")
     Call<ResponseBody> loadCityWeather(@Query("APPID") String appId, @Query("units") String units, @Query("q") String city);
 
